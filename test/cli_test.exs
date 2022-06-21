a61e7eb790fa9cef545f52cd5c10bfcdf4af1b87
Hoon Wee <mrgravity817@gmail.com>
2022-06-21 20:18:19 +0900

    feat(cli): should sort in descending order

--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,7 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [parse_args: 1]
+  import Issues.CLI, only: [parse_args: 1, sort_into_descending_order: 1]
 
   test "If the options parses to -h or --help, then it would return :help." do
     assert parse_args(["-h", "anything"]) == :help
@@ -15,5 +15,16 @@ defmodule CliTest do
 
   test "When 2 values sent, use default count." do
     assert parse_args(["user", "project"]) == {"user", "project", 4}
+  end
+
+  test "Sorting descending order should work" do
+    result = sort_into_descending_order(fake_created_at_list(["c", "a", "b"]))
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+    assert issues == ~w{c b a}
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+        do: %{"created_at" => value, "other_data" => "xxx"}
   end
 end
