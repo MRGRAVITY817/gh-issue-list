f250e4dcce86bab13385688e110942b91fb5637c
Hoon Wee <mrgravity817@gmail.com>
2022-06-21 17:04:01 +0900

    feat(cli): add json parser

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -10,13 +10,10 @@ defmodule Issues.GithubIssues do
     "https://api.github.com/repos/#{user}/#{project}/issues"
   end
 
-  # if succeeded
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body}
+  def handle_response({_, %{status_code: status_code, body: body}}) do
+    {status_code |> check_for_error, body |> Poison.Parser.parse!(%{})}
   end
 
-  # if failed
-  def handle_response({_, %{status_code: _, body: body}}) do
-    {:error, body}
-  end
+  defp check_for_error(200), do: :ok
+  defp check_for_error(_), do: :error
 end
