7186c04b26497fa6f3b863240567022667824019
Hoon Wee <mrgravity817@gmail.com>
2022-06-21 23:47:16 +0900

    log(github_issues): add logging for Issues.GithubIssues module

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,9 @@
 import Config
 
 config :issues, github_url: "https://api.github.com"
+
+# this will purge the logs with level lower than `info`
+config :logger,
+  compile_time_purge_matching: [
+    [level_lower_than: :info]
+  ]
